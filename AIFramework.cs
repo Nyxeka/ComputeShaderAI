cf51be3050f82f816ca1b88fd7c44da3a03a8cca
Nicholas <njhylands@gmail.com>
2018-07-02 15:15:30 -0400

    Implemented first kernel for texture-based neural network running successfully on GPU, stores model in another texture

---
+++ AIFramework.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nyxeka.ai
+{
+	
+
+
+	public enum AIStep{
+		addWeights,sumWeights,createOutput
+	}
+
+    public class AIFramework : MonoBehaviour
+    {
+        /// <summary>
+        /// 32x32 visual input.
+        /// </summary>
+        public RenderTexture visionInput;
+        /// <summary>
+        /// visual processing results to be used with activation function.
+        /// </summary>
+        private Texture2D visionOutput;
+        /// <summary>
+        /// input weights for our processing.
+        /// </summary>
+        public Texture2D weights;
+        /// <summary>
+        /// weighted value processor.
+        /// </summary>
+        protected Texture2D weightedValues;
+
+		//protected Color32[,] texture;
+
+        public ComputeShader NNProcessor;
+
+		Color32[] pixels;
+
+		Color32 activatedStepA;
+        
+        // Use this for initialization
+        void Start()
+        {
+            visionInput.enableRandomWrite = true;
+			visionOutput = new Texture2D (AITexSizes.INPUT_TEX_WIDTH, AITexSizes.INPUT_TEX_WIDTH);
+			weightedValues = new Texture2D (AITexSizes.WEIGHTED_VALUE_HOLDER_WIDTH, AITexSizes.WEIGHTED_VALUE_HOLDER_HEIGHT);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void RunProcess()
+        {
+
+        }
+
+        void StopProcess() { }
+
+        IEnumerator VisualProcessing()
+		{
+			// "weight" as a verb.
+			int kernelWeightValues = NNProcessor.FindKernel (KernelNames.WeightValues);
+
+			int kernelSumValues = NNProcessor.FindKernel (KernelNames.SumWeights);
+
+			int kernelCreateOutput = NNProcessor.FindKernel (KernelNames.CreateOutput);
+
+			int kernelVisProcess = NNProcessor.FindKernel ("NNProcessVisuals");
+
+			ComputeBuffer buffer = new ComputeBuffer (1024, 16);
+			
+			pixels = visionOutput.GetPixels32();
+
+			buffer.SetData (pixels);
+
+			Color32[] output = new Color32[1024];
+			/*
+			while (true) {
+			//	*/
+
+				//start by doing a dispatch
+				NNProcessor.SetTexture(kernelWeightValues,KernelNames.VISION_IO_BUFFER,visionInput);
+				NNProcessor.SetBuffer(kernelVisProcess, "output", buffer);
+				NNProcessor.Dispatch (kernelWeightValues, KernelNames.stepA_X, KernelNames.stepA_Y, KernelNames.stepA_Z);
+
+				//------
+				yield return null;
+				//grab the information:
+				
+				buffer.GetData (output);
+
+				visionOutput.SetPixels32 (output);
+
+				visionOutput.Apply ();
+
+				activatedStepA = visionOutput.GetPixels32 (5) [0];
+				/*
+			}
+			// */
+        }
+
+
+
+    }
+}
---
+++ KernelNames.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nyxeka.ai
+{
+
+    public static class KernelNames
+    {
+		//names of functions
+        public static string WeightValues = "NNAddWeights1024";
+        public static string SumWeights = "NNSumWeights256";
+        public static string CreateOutput = "NNCreateOutput";
+
+		//names of buffers
+		public static string VISION_IO_BUFFER = "Result";
+		public static string WEIGHT_INPUT = "weightValues";
+		public static string WEIGHTED_VALUE_HOLDER = "weightedValues";
+
+		public static int stepA_X = 1;
+		public static int stepA_Y = 4;
+		public static int stepA_Z = 64;
+
+		public static int stepB_X = 16;
+		public static int stepB_Y = 64;
+		public static int stepB_Z = 1;
+
+		public static int stepC_X = 1;
+		public static int stepC_Y = 1;
+		public static int stepC_Z = 1;
+        
+    }
+
+    public static class AITexSizes
+    {
+        public static int 
+            INPUT_TEX_WIDTH = 32,
+            OUTPUT_TEX_WIDTH = 32,
+			WEIGHTED_VALUE_HOLDER_WIDTH = 256,
+			WEIGHTED_VALUE_HOLDER_HEIGHT = 1024,
+            AXON_TEX_WIDTH = 1024,
+            STAT_INPUT_WIDTH = 8,
+            STAT_INFLUENCE_WIDTH = 8,
+            HORMONE_TEX_WIDTH = 8,
+            MEMORY_TEX_WIDTH = 8192,
+            NEW_THOUGHT_BUFFER = 2048;
+    }
+}
